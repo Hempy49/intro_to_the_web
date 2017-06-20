46dd2827d41cfe4cc60a83a948a64b0b5c2f2d39
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-20 11:05:15 +0100

    Add a form to view

--- app.rb
+++ app.rb
@@ -13,7 +13,12 @@ get '/secret' do
 	'This is a secret!'
 end
 
-get '/cat' do
+get '/random-cat' do
 	@random_names = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+	@name = params[:name]
   erb(:index)
 end
